03f0dbcbf7412b5abe492ab0ef1fd98cc133f51e
Bob <bob2build.2020@gmail.com>
2016-05-08 10:28:46 +0530

    Add serverpec integration tests for all platforms

---
+++ test/integration/default/serverspec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'serverspec'
+
+require 'serverspec'
+
+if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
+  set :backend, :exec
+else
+  set :backend, :cmd
+  set :os, family: 'windows'
+end
