afa99bbf1666f904af642beb92e41c4fb9e92017
Virender Khatri <vir.khatri@gmail.com>
2016-09-11 10:11:42 +0530

    update spec for v1.3.0

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -84,7 +84,7 @@ describe 'topbeat::default' do
     end
 
     it 'configure C:/opt/topbeat/topbeat.yml' do
-      expect(chef_run).to create_file('C:/opt/topbeat/topbeat-1.2.3-windows/topbeat.yml')
+      expect(chef_run).to create_file('C:/opt/topbeat/topbeat-1.3.0-windows/topbeat.yml')
     end
 
     it 'enable topbeat service' do
